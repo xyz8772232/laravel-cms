6be68c4eab839849573a51f77833277cb94c068a
kael <jiangshanyanyuwhn@gmail.com>
2016-10-26 10:10:21 +0800

    Merge branch 'master' of https://github.com/xyz8772232/laravel-admin
    # Conflicts:
    #	composer.lock

--- resources/views/admin/photo/index.blade.php
+++ resources/views/admin/photo/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
+11111
 @endsection
 
 @section('css')
-    <link rel="stylesheet" href="/css/index.css">
+    <!--<link rel="stylesheet" href="/css/index.css">-->
 @endsection
 
 @section('admin_js')
-    <script data-exec-on-popstate src="/js/index.js"></script>
+    <script data-exec-on-popstate src="/js/photo.index.js"></script>
 @endsection
