3cf160952b7f592fc438cde8b038283c746f381b
kael <jiangshanyanyuwhn@gmail.com>
2016-10-27 14:13:29 +0800

    1.素材管理

--- public/js/photo.index.js
+++ public/js/photo.index.js
@@ -1,10 +1,114 @@
-/**
- * Copyright 2016-present, Sina, Inc.
- * All rights reserved.
- *
- * @author kael
- * @create 2016-10-25 21:11
- *
- * $DESCRIPTION$
- * @version $VERSION$
- */
+$(function () {
+  /**
+   * 选择
+   */
+  var $selectAll = $('.img-select-all');
+  var $selectList = $('.img-select');
+  var keyControlIsActive = false;
+  var keyShiftIsActive = false;
+  var selectInterval = [0, 0];
+
+  // 全选
+  $selectAll.change(function () {
+    if (this.checked) {
+      $selectList.prop("checked", true);
+    } else {
+      $selectList.prop("checked", false);
+    }
+    selectInterval[0] = 0;
+  });
+
+  // 选中(支持ctrl + shift)
+  $selectList.on('change', function () {
+    var $this = $(this);
+    if (keyControlIsActive) {
+      console.log('control');
+      selectInterval[0] = +$this.attr('data-index');
+    } else if (keyShiftIsActive) {
+      console.log('shift');
+      var selectIntervalBySort;
+      $selectList.prop("checked", false);
+      selectInterval[1] = +$this.attr('data-index');
+      console.log(selectInterval);
+      selectIntervalBySort = selectInterval.slice().sort(function (a, b) {
+        return a - b;
+      });
+      $(Array.prototype.slice.call($selectList, selectIntervalBySort[0], selectIntervalBySort[1] + 1)).prop("checked", true);
+    } else {
+      $selectList.prop("checked", false);
+      $this.prop("checked", true);
+      selectInterval[0] = +$this.attr('data-index');
+    }
+    $selectAll.prop("checked", false);
+  });
+
+  $(document).on('keydown', function (e) {
+    var keyCode = e.keyCode;
+    if (keyCode === 17 || (91 <= keyCode && keyCode <= 93)) {
+      keyControlIsActive = true;
+    } else if (keyCode === 16) {
+      keyShiftIsActive = true;
+    }
+  }).on('keyup', function (e) {
+    var keyCode = e.keyCode;
+    if (keyCode === 17 || (91 <= keyCode && keyCode <= 93)) {
+      keyControlIsActive = false;
+    } else if (keyCode === 16) {
+      keyShiftIsActive = false;
+    }
+  });
+
+  /**
+   * 复制地址
+   */
+  $('.box-body').on('click', '.img-copy-url', function (e) {
+    var url = this.parentNode.previousElementSibling.src;
+    console.log(e);
+    if (e.clipboardData) {
+      e.clipboardData.setData("text/plain", url);
+    } else if (window.clipboardData) {
+      window.clipboardData.setData("text", url);
+    } else {
+      alert('当前浏览器不支持直接复制,请手动复制:' + url);
+      return false;
+    }
+    alert('复制成功');
+  });
+
+  /**
+   * 上传新图片
+   */
+  $('.img-upload').on('click', function () {
+    swal({
+      title: '上传',
+      text: '<input type="file" id="cover_pic" name="cover_pic"/><input type="hidden" id="cover_pic_action" name="cover_pic_action" value="0"/>',
+      html: true,
+      allowOutsideClick: true,
+      confirmButtonText: "上传"
+    }, function(isConfirm){
+      console.log(isConfirm);
+    });
+    $('#cover_pic').fileinput({
+      "overwriteInitial": true,
+      "showUpload": false,
+      "language": "zh_CN",
+      "allowedFileTypes": ["image"],
+      "initialCaption": "",
+      minFileCount: 0
+    }).on('filecleared', function (event) {
+      $("#cover_pic_action").val(1);
+    });
+  });
+
+  function upload() {
+    
+  }
+
+  function enableImgUpload() {
+    
+  }
+
+  function disableImgUpload() {
+    
+  }
+});
--- resources/views/admin/photo/index.blade.php
+++ resources/views/admin/photo/index.blade.php
@@ -1,13 +1,168 @@
 @extends('layouts.app')
 
 @section('content')
-11111
+<section class="content-header">
+  <h1>
+    header
+    <small>description</small>
+  </h1>
+</section>
+<section class="content">
+  <div class="box">
+    <div class="box-header">
+      <input type="checkbox" class="img-select-all"> 全选
+      <a class="img-delete btn btn-sm btn-danger">删除</a>
+      <a class="img-upload btn btn-sm btn-success">上传新图片</a>
+    </div>
+    <div class="box-body">
+      <div class="row">
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="0" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="1" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="2" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="3" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="4" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="5" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="6" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="7" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="8" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="9" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="10" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="img-box">
+            <img class="img"
+                 src="http://sinastorage.com/storage.caitou.sina.com.cn/products/201610/ec0ea3d929d5b3fdab13664d9f26389e.jpeg"
+                 alt="">
+            <div class="img-action">
+              <input type="checkbox" data-index="11" class="img-select input-lg">
+              <button type="button" class="img-copy-url btn btn-primary btn-xs pull-right">复制地址</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 @endsection
 
 @section('css')
-    <!--<link rel="stylesheet" href="/css/index.css">-->
+    <link rel="stylesheet" href="/css/photo.index.css">
+    <link rel="stylesheet" href="/packages/admin/sweetalert/sweetalert.css">
 @endsection
 
 @section('admin_js')
+    <script data-exec-on-popstate src="/packages/admin/sweetalert/sweetalert.min.js"></script>
     <script data-exec-on-popstate src="/js/photo.index.js"></script>
 @endsection
