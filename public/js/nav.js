283df6307bfc680068423107c55fafc89320a572
kael <jiangshanyanyuwhn@gmail.com>
2016-12-01 09:25:14 +0800

    1.侧边栏及头部

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,10 +2,8 @@ $(function () {
   /**
    * 用户
    */
-  var userMenuFold = true;
   $('#eUser').on('click', function () {
-    $(this).parent()[userMenuFold ? 'addClass' : 'removeClass']('unfold');
-    userMenuFold = !userMenuFold;
+    $(this).siblings('.user-menu').slideToggle(200);
   });
 
   /**
@@ -20,28 +18,31 @@ $(function () {
 
   // 一级菜单
   var $menu2List = $('.menu-2');
-  var $curMenu1 = $('.menu-1').filter('.active');
+  var $curMenu1 = $('.menu-1 .active');
   var $curMenu2 = $menu2List.filter('.active');
   $('#eMenu1').on('click', '.e-select', function () {
     var $this = $(this);
     var index = +this.getAttribute('data-index');
 
-    if ($this.hasClass('active')) return false;
-    $curMenu1.removeClass('active');
-    $curMenu1 = $this;
-    $curMenu1.addClass('active');
-    $curMenu2.removeClass('active');
-    $curMenu2 = $menu2List.eq(index);
-    $curMenu2.addClass('active');
+    if (!$this.hasClass('active')) {
+      $curMenu1.removeClass('active');
+      $curMenu1 = $this;
+      $curMenu1.addClass('active');
+      $curMenu2.removeClass('active');
+      $curMenu2 = $menu2List.eq(index);
+      $curMenu2.addClass('active');
+    }
   });
 
   // 二级菜单
-  $('#eMenu2').on('click', '.e-select', function () {
+  $('#eMenu2').on('click', '.e-select', function (e) {
     var $this = $(this);
 
-    if ($this.hasClass('active')) return false;
-
-    $this.addClass('active')
-    .siblings('.active').removeClass('active');
+    $this.toggleClass('active')
+    .children('.menu-3').slideToggle(250);
+    $this.siblings('.active').removeClass('active')
+    .children('.menu-3').slideUp(250);
+  }).on('click', '.menu-3', function (e) {
+    e.stopPropagation();
   });
 });
