93326a7f3b23c042edba7e36fbf737ea705d29ee
xiaotie <812250076@qq.com>
2016-12-22 14:16:08 +0800

    all 0.99

--- database/seeds/ChannelsTableSeeder.php
+++ database/seeds/ChannelsTableSeeder.php
@@ -15,23 +15,23 @@ class ChannelsTableSeeder extends Seeder
         \App\Channel::create(
             ['name' => '新闻', 'admin_user_id' => 1, 'grade' => 1, 'parent_id' => 0]
         );
-        \App\Channel::create(
-            ['name' => '新闻资讯', 'admin_user_id' => 1, 'grade' => 2, 'parent_id' => 1]
-        );
-        \App\Channel::create(
-            ['name' => '房产财经', 'admin_user_id' => 2, 'grade' => 2, 'parent_id' => 1]
-        );
-        \App\Channel::create(
-            ['name' => '纽澳新闻', 'admin_user_id' => 2, 'grade' => 3, 'parent_id' => 2]
-        );
-        \App\Channel::create(
-            ['name' => '环球报道', 'admin_user_id' => 2, 'grade' => 3, 'parent_id' => 2]
-        );
-        \App\Channel::create(
-            ['name' => '综合', 'admin_user_id' => 2, 'grade' => 4, 'parent_id' => 5]
-        );
-        \App\Channel::create(
-            ['name' => '社会', 'admin_user_id' => 2, 'grade' => 4, 'parent_id' => 5]
-        );
+//        \App\Channel::create(
+//            ['name' => '新闻资讯', 'admin_user_id' => 1, 'grade' => 2, 'parent_id' => 1]
+//        );
+//        \App\Channel::create(
+//            ['name' => '房产财经', 'admin_user_id' => 2, 'grade' => 2, 'parent_id' => 1]
+//        );
+//        \App\Channel::create(
+//            ['name' => '纽澳新闻', 'admin_user_id' => 2, 'grade' => 3, 'parent_id' => 2]
+//        );
+//        \App\Channel::create(
+//            ['name' => '环球报道', 'admin_user_id' => 2, 'grade' => 3, 'parent_id' => 2]
+//        );
+//        \App\Channel::create(
+//            ['name' => '综合', 'admin_user_id' => 2, 'grade' => 4, 'parent_id' => 5]
+//        );
+//        \App\Channel::create(
+//            ['name' => '社会', 'admin_user_id' => 2, 'grade' => 4, 'parent_id' => 5]
+//        );
     }
 }
