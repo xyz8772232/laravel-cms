93f5b0e3c8f6982ebff40cdc2fc64756c5195291
xiaotie <812250076@qq.com>
2017-01-17 13:44:35 +0800

    sort photo comment sortlink_withphoto

--- app/Api/Controllers/SortLinkController.php
+++ app/Api/Controllers/SortLinkController.php
@@ -29,7 +29,7 @@ class SortLinkController extends BaseController
             $photoArticles = $sortPhotos->map(function($sortPhoto) {
                 $sortPhoto->article->cover_pic = $sortPhoto->article->cover_pic ? image_url($sortPhoto->article->cover_pic) : null;
                 $sortPhoto->article->url = route('articles.show', ['id' => $sortPhoto->article->id]);
-                return $sortPhoto->article->toArray();
+                return $sortPhoto->article->makeVisible('comment_num')->toArray();
             })->all();
             $links->addMeta('photos', $photoArticles);
         }
